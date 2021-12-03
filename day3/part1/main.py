b23545c901b51b5b17c2d5105f0fe91f9f3728b7
Zamua Nasrawt <zamua@nasrawt.io>
2021-12-03 14:41:26 -0500

    1-linerify day3-part1

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -1,8 +1,5 @@
 import sys
 from collections import Counter
+from functools import reduce
 lines = sys.stdin.readlines()
-
-nums = [(c.most_common()[0][0], c.most_common()[-1][0]) for c in map(Counter, [[lines[row][col] for row in range(len(lines))] for col in range(len(lines[0]))])]
-gamma = int("".join([num[0] for num in nums]), 2)
-epsilon = int("".join([num[1] for num in nums]), 2)
-print(gamma*epsilon)
+print(reduce(lambda g, e: g*e, map(lambda s: int("".join(s), 2), reduce(lambda n, d: (n[0]+d[0], n[1]+d[1]), [(c.most_common()[0][0], c.most_common()[-1][0]) for c in map(Counter, [[lines[row][col] for row in range(len(lines))] for col in range(len(lines[0]))])])))[0])
